dfe8d2bac2c9703ab5e900da79659a4c0c903223
CosmicHigh <soutiksarkar02@gmail.com>
2026-03-23 14:59:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/meditrack/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.meditrack;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        // This MUST match your file name in the assets folder exactly
+        webView.loadUrl("file:///android_asset/MediTrack.html");
+
+        // The modern way to handle the back button
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if (webView.canGoBack()) {
+                    webView.goBack();
+                } else {
+                    finish();
+                }
+            }
+        });
+    }
+}
